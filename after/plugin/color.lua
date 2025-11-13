67b0f5585ee79d4ef79287ba4939f3384746ef1f
Torbjørn Rolstad <torbjrol@stud.ntnu.no>
2025-11-13 12:27:24 +0100

    trim down packer and adjust colorscheme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -8,13 +8,23 @@ require("rose-pine").setup({
     variant = "moon",
     highlight_groups = {
         Normal = { bg = dark_bg },
+        NormalNC = { bg = dark_bg },
         Comment = { fg = grey },
+        -- Syntax
         ["@variable.member"] = { fg = rose_pine_moon_variabel_member_custom },
         ["@function.method.call"] = { fg = rose_pine_moon_rose },
         ["@property"] = { fg = rose_pine_moon_variabel_member_custom },
         -- TODO: double check if attributes are needed
         ["@attribute"] = { fg = rose_pine_moon_variabel_member_custom },
         ["@attribute.builtin"] = { fg = rose_pine_moon_variabel_member_custom },
+        -- Telescope
+        NormalFloat = { bg = dark_bg },
+        FloatBorder = { bg = dark_bg, fg = grey },
+        TelescopeBorder = { bg = dark_bg, fg = grey },
+        TelescopeNormal = { bg = dark_bg },
+        TelescopePromptNormal = { bg = dark_bg },
+        TelescopeResultsNormal = { bg = dark_bg },
+        TelescopePreviewNormal = { bg = dark_bg },
   },
 })
 
--- lua/tbear/packer.lua
+++ lua/tbear/packer.lua
@@ -5,16 +5,6 @@
 
 return require("packer").startup(function(use)
   use { "wbthomason/packer.nvim" }
-  use { "ramojus/mellifluous.nvim" }
-  use { "wtfox/jellybeans.nvim" }
-  use { "savq/melange-nvim" }
-  use { "vague2k/vague.nvim" }
-  use { "rebelot/kanagawa.nvim" }
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use { "sainnhe/gruvbox-material" }
-  use { "morhetz/gruvbox" }
-  use { "olimorris/onedarkpro.nvim" }
-  use { "blazkowolf/gruber-darker.nvim" }
   use { "rose-pine/neovim", name = "rose-pine" }
   use { "nvim-telescope/telescope.nvim", tag = "0.1.8", requires = { {"nvim-lua/plenary.nvim"} } }
   use { "theprimeagen/harpoon" }
